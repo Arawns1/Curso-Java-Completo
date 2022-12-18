a720ebcba3fa09f6047c102d977b5672df1f5134
Gabriel Damico <gabrieldamico22@gmail.com>
2022-12-18 18:01:33 -0300

    Base completa, implementando StringBuilder

--- Secao_13 - posts/src/Post.java
+++ Secao_13 - posts/src/Post.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Post {
 	Date moment;
@@ -7,6 +9,60 @@ public class Post {
 	Integer likes;
 	
 	List<Comment> comments = new ArrayList<>();
+	
+	
+	public Post(Date moment, String title, String content, Integer likes) {
+		this.moment = moment;
+		this.title = title;
+		this.content = content;
+		this.likes = likes;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+	
+	public void addComment(Comment comment) {
+		comments.add(comment);
+	}
+	public void removeComment(Comment comment) {
+		comments.remove(comment);
+	}
+
+
+	
 	
 	
 }
--- Secao_13 - posts/src/main.java
+++ Secao_13 - posts/src/main.java
@@ -1,8 +1,20 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
+		Comment c1 = new Comment("Have a nice tripe!");
+		Comment c2 = new Comment("Wow that's awesome!");
+		
+		Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"),"Traveling to New Zeland","Im going to visit wonderful contry", 12);
+		
+		p1.addComment(c1);
+		p1.addComment(c2);
 	}
 
 }
