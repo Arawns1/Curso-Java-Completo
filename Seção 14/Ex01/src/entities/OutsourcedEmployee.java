9c2b933081ea95ca62922f612a2d1b93f531c8e1
Gabriel Damico <gabrieldamico22@gmail.com>
2022-12-22 19:38:58 -0300

    OutsourcedEmployee criado

--- Seção 14/Ex01/src/entities/OutsourcedEmployee.java
+++ Seção 14/Ex01/src/entities/OutsourcedEmployee.java
@@ -1,6 +1,26 @@
 package entities;
 
-public class OutsourcedEmployee {
+public class OutsourcedEmployee extends Employee {
+	
+	private double additionalCharge;
+	
+	public OutsourcedEmployee(String name, int hours, double valuePerHour, double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
 
+
+	public double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+	
+	@Override
+	public double payment() {
+		return (this.getValuePerHour() * this.getHours()) + this.getAdditionalCharge()*1.10;
+	}
 	
 }
