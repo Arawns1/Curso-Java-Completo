5863b46d41ba012440cdb4ec3af843db46db11ac
Gabriel Damico <gabrieldamico22@gmail.com>
2022-12-22 20:43:42 -0300

    Criado ImportedProduct

--- Seção 14/Ex02/src/entities/ImportedProduct.java
+++ Seção 14/Ex02/src/entities/ImportedProduct.java
@@ -1,5 +1,23 @@
 package entities;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product {
+	private double customsFee;
 
+	public double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	public double totalPrice() {
+		return super.getPrice() + this.getCustomsFee();
+	}
+	
+	@Override
+	public String priceTag() {
+		return this.getName() + " $ " + this.getPrice() + "(Custom Fees = $"+ this.getCustomsFee()+")";
+	}
+	
 }
